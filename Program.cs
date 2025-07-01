f6df118e6245dda3762e7b104f1faf2e0a3381fb
Giorgi Oniani <giorgi_oniani@epam.com>
2025-07-01 19:54:19 +0400

    task1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.MapGet("/api/hello", () => "Hello, EDP!");
+var podName = Environment.GetEnvironmentVariable("HOSTNAME") ?? "unknown-host";
+app.MapGet("/api/hello", () => $"Hello, KubeRocketCI from {podName}");
 
 app.Run();
